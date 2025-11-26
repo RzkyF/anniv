1de4ed8bf57bb27b7e9a61fbc3150adb2ca74acf
RzkyF_ <rizkifadillah616@outlook.co.id>
2025-11-26 17:31:52 +0700

    ver 1

---
+++ public/js/config.js
@@ -0,0 +1,19 @@
+ tailwind.config = {
+        darkMode: "class",
+        theme: {
+          extend: {
+            colors: {
+              "primary": "#B76E79",
+              "background-light": "#F9F6F2",
+              "background-dark": "#221016",
+              "text-light": "#4A4A4A",
+              "text-dark": "#D8C2C0",
+            },
+            fontFamily: {
+              "display": ["Epilogue", "sans-serif"],
+              "serif-display": ["Playfair Display", "serif"],
+            },
+            borderRadius: {"DEFAULT": "1rem", "lg": "2rem", "xl": "3rem", "full": "9999px"},
+          },
+        },
+      }
